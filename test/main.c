c055d00253cbe3f59048a652568982feeff8ccff
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-25 23:45:55 +0100

    checking

--- test/main.c
+++ test/main.c
@@ -69,5 +69,9 @@ int main(void)
     printf("Unknown:[%r]\n");
     _printf("Binary:[%b]\n", 98);
     printf("Binary:[%b]\n", 98);
+    _printf("Decimal:[%d%d]\n", 98);
+    printf("Decimal:[%d%d]\n", 98);
+    _printf("Decimal:[%i]\n", 524);
+    printf("Decimal:[%i]\n", 524);
     return (0);
 }
